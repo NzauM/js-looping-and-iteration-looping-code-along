c805e185dddef24dd29dbc78971ef9576223db79
mercy.waeni <mercy.waeni@calltronix.com>
2022-01-25 14:34:10 +0300

    complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages = []
+    for(let i=0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        // return(`Thank you, ${names[i]}, for the wonderful birthday gift!`)
+        // console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!`)
+    }
+    return messages
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num)
+        num = num-1
+    }
+}
